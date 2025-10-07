b712c460333e8d936aaf16da7a3ef1b60e31952d
Eigeen <dengyk2002@qq.com>
2025-10-07 16:13:05 +0800

    update workflow to complile more os and arch

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
     if let Ok(target) = std::env::var("TARGET") {
         println!("cargo:rustc-env=COMPILED_TARGET_TRIPLE={target}");
     }
